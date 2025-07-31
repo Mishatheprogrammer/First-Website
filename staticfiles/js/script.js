f3c04eb21bf5f0076820811e320b2cceaa9fd9dd
Your Name <you@example.com>
2025-07-31 15:33:42 -0400

    deployment

---
+++ staticfiles/js/script.js
@@ -0,0 +1,66 @@
+// some scripts
+
+// jquery ready start
+$(document).ready(function() {
+	// jQuery code
+
+
+    /* ///////////////////////////////////////
+
+    THESE FOLLOWING SCRIPTS ONLY FOR BASIC USAGE, 
+    For sliders, interactions and other
+
+    */ ///////////////////////////////////////
+    
+
+	//////////////////////// Prevent closing from click inside dropdown
+    $(document).on('click', '.dropdown-menu', function (e) {
+      e.stopPropagation();
+    });
+
+
+    $('.js-check :radio').change(function () {
+        var check_attr_name = $(this).attr('name');
+        if ($(this).is(':checked')) {
+            $('input[name='+ check_attr_name +']').closest('.js-check').removeClass('active');
+            $(this).closest('.js-check').addClass('active');
+           // item.find('.radio').find('span').text('Add');
+
+        } else {
+            item.removeClass('active');
+            // item.find('.radio').find('span').text('Unselect');
+        }
+    });
+
+
+    $('.js-check :checkbox').change(function () {
+        var check_attr_name = $(this).attr('name');
+        if ($(this).is(':checked')) {
+            $(this).closest('.js-check').addClass('active');
+           // item.find('.radio').find('span').text('Add');
+        } else {
+            $(this).closest('.js-check').removeClass('active');
+            // item.find('.radio').find('span').text('Unselect');
+        }
+    });
+
+
+
+	//////////////////////// Bootstrap tooltip
+	if($('[data-toggle="tooltip"]').length>0) {  // check if element exists
+		$('[data-toggle="tooltip"]').tooltip()
+	} // end if
+
+
+
+
+    
+}); 
+// jquery end
+setTimeout(function(){
+    $('#message').fadeOut('slow')
+}, 5000) // your miliseconds after the comma
+
+// To apply it on your web-page click 'Inspect Page' of your website and then right-click on the 'Refresh' button
+// Then press 'Empty cache and hard reload', now register a user and it should work
+// Because what you did is your updated jQuery
